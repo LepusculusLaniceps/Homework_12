b4f4fc42eabd15a26c45c87f71e4dc325e8b6d82
Yuna <ksedih@rambler.ru>
2023-08-06 01:02:48 +0300

    Homework_12

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public class Book {
+        String authorName;
+        String bookName;
+        int publishingYear;
+    }
+    public static void main(String[] args) {
+
+    }
+}
