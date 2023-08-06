0c5875bd64707adc4ba035025ee13d0326cf5152
Yuna <ksedih@rambler.ru>
2023-08-06 03:15:26 +0300

    Homework_12 was difficult, but really interesting. I adjusted my code to Gregory's analysis. I'll practice on my own during the holidays - I need to wrap my head around the methods.

--- src/pro/sky/java/course1/homework12/Main.java
+++ src/pro/sky/java/course1/homework12/Main.java
@@ -10,12 +10,17 @@ public class Main {
         Book flowersForAlgernon = new Book("Цветы для Элджернона", keyes, 1966);
         Book nineteenEightyFour = new Book("1984", orwell, 1956);
 
-        System.out.println(ofMiceAndMen.getName() + " " + ofMiceAndMen.getPublicationYear() + " " + ofMiceAndMen.getAuthor().getFirstName() + " " + ofMiceAndMen.getAuthor().getSurname());
-        System.out.println(flowersForAlgernon.getName() + " " + flowersForAlgernon.getPublicationYear() + " " + flowersForAlgernon.getAuthor().getFirstName() + " " + flowersForAlgernon.getAuthor().getSurname());
-        System.out.println(nineteenEightyFour.getName() + " " + nineteenEightyFour.getPublicationYear() + " " + nineteenEightyFour.getAuthor().getFirstName() + " " + nineteenEightyFour.getAuthor().getSurname());
-        nineteenEightyFour.setPublicationYear(2020);
-        System.out.println("nineteenEightyFour.getPublicationYear() = " + nineteenEightyFour.getPublicationYear());
+        printBook(ofMiceAndMen);
+        printBook(flowersForAlgernon);
+        printBook(nineteenEightyFour);
 
+
+        nineteenEightyFour.setPublicationYear(2020);
+        printBook(nineteenEightyFour);
+
+    }
+    private static void printBook(Book book) {
+        System.out.println(book.getName() + " " + book.getPublicationYear() + " " + book.getAuthor().getFirstName() + " " + book.getAuthor().getSurname());
 
     }
 }
